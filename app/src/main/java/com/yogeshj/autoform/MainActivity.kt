8e6c6ba98f3bddac02ac318d45462a7dd9d4cafe
YogeshJsw <yogeshjaiswal.0811@gmail.com>
2024-08-15 19:22:18 +0530

    Commit

---
+++ app/src/main/java/com/yogeshj/autoform/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.yogeshj.autoform
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yogeshj.autoform.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        startActivity(Intent(this@MainActivity,LoginActivity::class.java))
+    }
+}
